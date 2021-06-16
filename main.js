3efc381831af98212cf5c025399039a8818f456a
SHUBHTHAREJA <shubhthareja12@gmail.com>
2021-06-16 19:56:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-video="";
-
+status="";
+objects=[];
 function preload(){
    video= createVideo('video.mp4');
-   video.hide();
 }
  
  function setup(){
    canvas= createCanvas(500,450);
    canvas.position(470,250);
+   video.hide();
    }
-
-   function draw(){
-     image(video,0,0,500,450);
-   }
-
 
    function start(){
-     objectDetector = ml5.objectDetector("cocossd",modelLoaded)
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
      document.getElementById("status").innerHTML= "status : detecting objects"
    }
 
@@ -27,3 +22,34 @@ function preload(){
     video.speed(1);
     video.volume(0);
    }
+
+
+function gotResults(error,results){
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    objects=results;
+  }
+}
+
+
+function draw(){
+  image(video,0,0,500,450);
+
+  if (status != "") {
+    
+    objectDetector.detect(video,gotResults);
+    for (i =0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML= "status : Object Detected"
+      document.getElementById("number_of_objects").innerHTML="number of object detected are:"+ objects.length;
+      
+      fill("cyan");
+      percantage=Math.floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percantage + "%",objects[i].x + 15,objects[i].y + 15);
+      nofill();
+      stroke("cyan");
+      rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+  }
+}
